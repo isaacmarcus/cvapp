177e34599d3b34ffe5537d86066d2dd9ef998b26
isaacmarcus <isaac.marcus.lam@gmail.com>
2021-05-12 17:14:52 +0800

    flutter web build for latest commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a14fef9d686c0a28378fb3cfe0be5341",
-"/": "a14fef9d686c0a28378fb3cfe0be5341",
-"main.dart.js": "7afec54cf28c65dfbc539e891d80a360",
+"index.html": "7f591da4e841621df814fd0bd4d9d778",
+"/": "7f591da4e841621df814fd0bd4d9d778",
+"main.dart.js": "1bc2d8ea29601d7d10d367be0dc871cd",
 "manifest.json": "14fd2a10b8b4ab98d1a972163af47896",
 "version.json": "06c3752758cc0b85d2be65f4910be230"
 };
